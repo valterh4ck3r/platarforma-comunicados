38496d82c4a4a59db76e31497213d30723fa8042
Leandro <lhcb@a.recife.ifpe.edu.br>
2018-11-18 16:30:10 -0300

    Campo de data
    exemplo dt_criacao = "2015-01-12 13:36:53"

--- src/main/java/br/com/pdws/comunica/Usuario.java
+++ src/main/java/br/com/pdws/comunica/Usuario.java
@@ -6,6 +6,7 @@
 package br.com.pdws.comunica;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Column;
@@ -19,7 +20,10 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -68,6 +72,22 @@ public abstract class Usuario implements Serializable {
     @Column(name = "TXT_SENHA", length = 30, nullable = false)
     protected String senha;
   
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "DT_CRIACAO")
+    protected Date dataCriacao;    
+    
+    @PrePersist
+    protected void setDataCriacao() {
+        this.setDataCriacao(new Date());
+    }
+    
+     public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
     public Long getID() {
         return ID;
     }
